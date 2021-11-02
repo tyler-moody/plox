f61b0b48122e5d907ad5304953bcc9842ed2d78d
Tyler Moody <tmoody@qumulo.com>
2021-11-02 10:24:27 -0700

    break error handling into its own file

---
+++ error.py
@@ -0,0 +1,6 @@
+def report(line: int, where: str, message: str) -> None:
+    print(f'line {line} error {where}: {message}')
+
+
+def error(line: int, message: str) -> None:
+    return (line, '', message)
